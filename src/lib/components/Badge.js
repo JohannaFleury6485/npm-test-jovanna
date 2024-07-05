233b85326946be46a6183394022cbc706c7f36d0
Jovanna <79448670+beginner6485@users.noreply.github.com>
2024-07-05 16:27:48 +0200

    new version

--- src/lib/components/Badge.js
+++ src/lib/components/Badge.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React , { useState }  from 'react';
 import './badge.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 function Badge(){
+   const [isVisible, setIsVisible] = useState(true);
+
+   const handleClose = () => {
+      setIsVisible(false)
+      
+   }
    return (
-      <div>
-      <p className='validation'>Congrats, your answers have been validated! </p>
-      </div>
-     )
+      <>
+      {isVisible && (
+         <div className='badge-container'>
+            <FontAwesomeIcon icon={faTimes} onClick={handleClose} className='close-icon' />
+            <p className='validation'>Congrats, your answers have been validated!</p>
+         </div>
+      )}
+   </>
+);
 }
-   
 
 export default Badge;
